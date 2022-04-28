df2514ab2cb3f4c9af6493d9b2551de296a78c13
lkuffo <lkuffo@espol.edu.ec>
2022-04-28 16:41:57 +0200

    Test upload

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = '/announcements'
 
 load_dotenv()
 app = Flask('app')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ####################
 # ENDPOINTS
@@ -15,6 +20,17 @@ def vxml():
     response = make_response(template)
     response.headers['Content-Type'] = 'application/xml'
     return response
+
+
+@app.route('/announcement', methods=['POST'])
+def upload_voice():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return 'not good', 500
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return 'ok', 200
 
 
 if __name__ == "__main__":
